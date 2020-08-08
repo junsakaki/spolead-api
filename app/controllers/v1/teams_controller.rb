b2c649d68908a55384bf5fab1604b5ce2bbbe8e3
satoshi <urano3104@gmail.com>
2020-08-08 12:32:37 +0900

    minimum inplements TEAMs route and controller

--- app/controllers/v1/teams_controller.rb
+++ app/controllers/v1/teams_controller.rb
@@ -3,5 +3,17 @@ module V1
     def index 
       render json: Team.all, each_serializer: V1::TeamSerializer
     end
+
+    # to do should be added mail_address into Teams Table
+    def create
+      binding.pry
+      # binding.pry
+      # team = Team.new(params[:team])
+      # if team.save
+      #   # hogehoge
+      # else
+      #   # hogehoge
+      # endS
+    end
   end
 end
