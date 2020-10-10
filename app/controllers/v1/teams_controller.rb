4f8ad799861352d110a50b4710ab3172b6be2fd7
satoshi <urano3104@gmail.com>
2020-10-10 10:23:28 +0900

    add search function

--- app/controllers/v1/teams_controller.rb
+++ app/controllers/v1/teams_controller.rb
@@ -1,27 +1,39 @@
 module V1
   class TeamsController < ApplicationController
-    PAGE_PER = 5
+    # want to add Pagenation latar
+    # include Pagenation
+    # PAGE_PER = 5
 
     def index 
       if params[:sports_id]
-        # get Teams related to sportsId
-        team = Team.where(sports_id: params[:sports_id])
+        # get Teams filtered by sportsId
+        teams = Team.where(sports_id: params[:sports_id])
+        
       elsif params[:city_code]
-        # get Teams related to cityCodes
-        team = Team.where(city_code: params[:city_code])
+        # get Teams filtered by cityCodes
+        teams = Team.where(city_code: params[:city_code])
       end
       
-      # paginage by kaminari
-      paginated_team = team.page(params[:page]).per(PAGE_PER)
+      # extract by search_word
+      if(!!params[:search_word])
+        teams = teams.search_columns(params[:search_word])
+      end
 
-      render json: paginated_team, each_serializer: V1::TeamSerializer
+      # want to add function paginage by kaminari
+      # paginated_teams = teams.page(params[:page]).per(PAGE_PER)
+      # pagenation = resources_with_pagination(paginated_teams)
+      # json = V1::TeamSerializer.new(paginated_teams).serializable_hash.merge(pagenation)
+      # binding.pry
+      # render_200(json)
+      # render json: paginated_teams, each_serializer: V1::TeamSerializer
+
+      render json: teams, each_serializer: V1::TeamSerializer
     end
     
     def show
       render json: Team.find_by(id: params[:team_id]), each_serializer: V1::TeamSerializer
     end
 
-    # to do should be added mail_address into Teams Table
     def create
       team = Team.new(teams_params)
       if team.save
@@ -62,7 +74,8 @@ module V1
           :team_type, 
           :target_age_type, 
           :team_information,
-          :page         
+          :search_word,
+          :page
         )
     end
 
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,23 @@
 class Team < ApplicationRecord
   has_many :reviews
+
+  scope :search_columns, ->(search_word) do
+    where('name like ?', "%#{search_word}%").
+      or(where('prefecture like ?', "%#{search_word}%")).
+        or(where('city like ?', "%#{search_word}%")).
+          or(where('street_number like ?', "%#{search_word}%"))
+  end
+  
+  # want to refactor with using below 
+
+  # scope :search_columns, ->(columns, params) do
+  #   search_word = "%#{params}%"
+  #   conditon = nil
+  #   columns.each do |column|
+  #     condition = condition.nil? ? arel_table[column].matches(search_word) : condition.or(arel_table[column].matches(search_word))
+  #   end
+  
+  #   where(condition)
+  # end
+  
 end
