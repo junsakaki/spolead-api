cc1be899a32ded68a285f860d09e28dc303df9a1
satoshi <urano3104@gmail.com>
2020-11-16 22:22:24 +0900

    add allow cors

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,16 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Permit cross origin
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
+
   end
 end
