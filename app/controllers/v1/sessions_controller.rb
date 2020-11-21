28f5db04c0881ca0ae293373f378ccc7c3395f31
satoshi <urano3104@gmail.com>
2020-11-21 13:50:33 +0900

    implements login error handling for the being

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,19 +1,27 @@
 module V1
   class SessionsController < ApplicationController
-    skip_before_action :authenticate_user_from_token!
+    # skip_before_action :authenticate_user_from_token!
 
     # POST /v1/login 
     # example USER --- user@example.com/ mypass
     def create
       @user = User.find_for_database_authentication(email: params[:email])
-      return invalid_email unless @user
 
-      if @user.valid_password?(params[:password])
-        sign_in :user, @user
-        render json: @user, serializer: V1::SessionSerializer, root: nil
-      else
-        invalid_password
+      # once error handle
+      # return invalid_email unless @user
+      # render json: { status: 500, message: 'メールアドレスが存在しません。' } unless @user
+      if @user
+        if @user.valid_password?(params[:password])
+          sign_in :user, @user
+          render json: @user, serializer: V1::SessionSerializer, root: nil
+        else
+          invalid_password
+          render status: 401, json: { status: 401, message: 'パスワードが一致しません。' }
+        end
+      else 
+        render status: 401, json: { status: 401, message: 'メールアドレスが存在しません' }
       end
+
     end
 
     private
