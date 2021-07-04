a12aeae33682855f05808155b65759705ce1e544
junsakaki <57080866+junsakaki@users.noreply.github.com>
2021-07-04 16:35:14 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TERNFNLL4D"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TERNFNLL4D');
+</script>
+
 <template>
   <v-layout
     column
