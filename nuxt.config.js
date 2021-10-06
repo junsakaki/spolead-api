3f371605ad0c08c71a50daf09ef0704bf79222e7
ririki-nakabayashi <ririki.nakabayashi.force@gmail.com>
2021-10-06 21:01:48 +0900

    fix: サービス名称を「spolead」から「SpoLeadeR」へ変更する

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
       lang: 'ja',
       prefix: 'og: http://ogp.me/ns#'
     },
-    titleTemplate: '%s' + process.env.npm_package_name,
+    titleTemplate: '%s' + 'SpoLeadeR',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
