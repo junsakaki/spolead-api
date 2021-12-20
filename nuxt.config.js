7f9dcb0f47dd5153c97ece3e2ecf25b624774f8c
j-kubo <j-kubo@keylink.co.jp>
2021-12-21 08:23:29 +0900

    OGP設定メタディスクリプション

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '総合 スポーツチーム・スクール口コミ情報ポータル' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Spoleader' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://spoleader.com/' },
+      { hid: 'og:title', property: 'og:title', content: 'Spoleader' },
+      { hid: 'og:description', property: 'og:description', content: '総合 スポーツチーム・スクール口コミ情報ポータル' },
+      { hid: 'og:image', property: 'og:image', content: 'https://spoleader.com/_nuxt/img/top.ab38fa7.jpg' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
