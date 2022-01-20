55c056fe4a53bb07763f85f0be207fff1bb90759
j-kubo <j-kubo@keylink.co.jp>
2022-01-20 11:40:20 +0900

    広告修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,15 +8,18 @@
     </v-content>
     <ScrollTopButton />
     <v-footer padless>
-      <div class="a8"><a href="https://px.a8.net/svt/ejp?a8mat=3HP98U+35PTBM+4OIY+HWPVL" rel="nofollow">
-        <img border="0" width="240" height="120" alt="" src="https://www22.a8.net/svt/bgt?aid=211130382191&wid=001&eno=01&mid=s00000021841003008000&mc=1"></a>
-        <img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=3HP98U+35PTBM+4OIY+HWPVL" alt="">
-        <a href="https://px.a8.net/svt/ejp?a8mat=3HP98U+3GFM7M+4JD4+62MDD" rel="nofollow">
-        <img border="0" width="240" height="120" alt="" src="https://www25.a8.net/svt/bgt?aid=211130382209&wid=001&eno=01&mid=s00000021172001020000&mc=1"></a>
-        <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3HP98U+3GFM7M+4JD4+62MDD" alt="">
-        <a href="https://px.a8.net/svt/ejp?a8mat=3HP98U+3GFM7M+4JD4+62MDD" rel="nofollow">
-        <img border="0" width="468" height="120" alt="" src="https://www20.a8.net/svt/bgt?aid=211130382206&wid=001&eno=01&mid=s00000005249001004000&mc=1"></a>
-        <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3HP98U+3ENBEA+14I2+5Z6WX" alt="">
+        <a href="https://px.a8.net/svt/ejp?a8mat=3HP98U+2FIQPE+4ABU+6CP0X" rel="nofollow">
+        <img border="0" width="430" height="55" alt="" src="https://www20.a8.net/svt/bgt?aid=211130382147&wid=001&eno=01&mid=s00000020001001067000&mc=1"></a>
+        <img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=3HP98U+2FIQPE+4ABU+6CP0X" alt="">
+        <a href="https://px.a8.net/svt/ejp?a8mat=3HP98U+3ENBEA+14I2+5YRHD" rel="nofollow">
+        <img border="0" width="430" height="55" alt="" src="https://www23.a8.net/svt/bgt?aid=211130382206&wid=001&eno=01&mid=s00000005249001002000&mc=1"></a>
+        <img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=3HP98U+3ENBEA+14I2+5YRHD" alt="">
+        <a href="https://px.a8.net/svt/ejp?a8mat=3HP98U+35PTBM+4OIY+HVV0H" rel="nofollow">
+        <img border="0" width="430" height="55" alt="" src="https://www21.a8.net/svt/bgt?aid=211130382191&wid=001&eno=01&mid=s00000021841003004000&mc=1"></a>
+        <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3HP98U+35PTBM+4OIY+HVV0H" alt="">
+        <a href="https://px.a8.net/svt/ejp?a8mat=3HP98U+3GFM7M+4JD4+5ZEMP" rel="nofollow">
+        <img border="0" width="430" height="55" alt="" src="https://www25.a8.net/svt/bgt?aid=211130382209&wid=001&eno=01&mid=s00000021172001005000&mc=1"></a>
+        <img border="0" width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=3HP98U+3GFM7M+4JD4+5ZEMP" alt="">
       </div>
       <div class="footer-links">
         <div v-for="link in filteredFooterLinks()" :key="link.title" class="link white--text">
