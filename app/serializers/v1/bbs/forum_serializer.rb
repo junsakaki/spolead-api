302b6eadf9e09c2ec47914d741f90a048254d3f1
j-ikeda01 <you@example.com>
2022-01-22 13:34:03 +0900

    fix: フォーラム取得時に紐づくコメントのカウントが正常に行われていなかったため

--- app/serializers/v1/bbs/forum_serializer.rb
+++ app/serializers/v1/bbs/forum_serializer.rb
@@ -9,5 +9,13 @@ module V1
       :comments_count
 
     has_many :bbs_threads, serializer: V1::Bbs::ThreadSerializer, key: :thread, if: -> { instance_options[:children] }
+
+    def comments_count
+      counter = 0
+      object.bbs_threads.each do |thread|
+        counter += BbsComment.where(thread_id: thread.id).size
+      end
+      counter
+    end
   end
 end
