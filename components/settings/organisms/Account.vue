78cdfa1fbf10ec6b2f18ff58dbfd8ac75bf2b305
ririki-nakabayashi <ririki.nakabayashi.force@gmail.com>
2022-01-31 14:01:41 +0900

    fix(setting): ログイン前のヘッダーレイアウト崩れを修正

--- components/settings/organisms/Account.vue
+++ components/settings/organisms/Account.vue
@@ -1,142 +1,16 @@
 <template>
-  <div>
-    <div class="subtitle-1 font-weight-bold">
-      パスワードをリセットする
-    </div>
-    <v-form
-      ref="form"
-      v-model="valid"
-      :class="`${!$vuetify.breakpoint.smAndDown && 'reset-form'} ml-4 mt-2`"
-      lazy-validation
-    >
-      <div :class="`d-flex align-center caption ${!$vuetify.breakpoint.smAndDown && 'pb-4'}`">
-        現在のパスワード
-      </div>
-      <v-text-field
-        v-model="password.current"
-        required
-        placeholder="現在のパスワードを入力"
-        class="ma-0 pa-0"
-        :append-icon="passwordDisplay.current ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="passwordDisplay.current ? 'text' : 'password'"
-        @click:append="passwordDisplay.current = !passwordDisplay.current"
-      />
-      <div :class="`d-flex align-center caption ${!$vuetify.breakpoint.smAndDown && 'pb-4'}`">
-        新しいパスワード
-      </div>
-      <v-text-field
-        v-model="password.new"
-        :rules="passwordRules"
-        required
-        placeholder="新しいパスワードを入力"
-        class="ma-0 pa-0"
-        :append-icon="passwordDisplay.new ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="passwordDisplay.new ? 'text' : 'password'"
-        @click:append="passwordDisplay.new = !passwordDisplay.new"
-      />
-      <div :class="`d-flex align-center caption ${!$vuetify.breakpoint.smAndDown && 'pb-4'}`">
-        新しいパスワード（確認用）
-      </div>
-      <v-text-field
-        v-model="password.confirm"
-        :rules="passwordConfirmRules"
-        required
-        placeholder="新しいパスワードを入力（確認用）"
-        class="ma-0 pa-0"
-        :append-icon="passwordDisplay.confirm ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="passwordDisplay.confirm ? 'text' : 'password'"
-        @click:append="passwordDisplay.confirm = !passwordDisplay.confirm"
-      />
-    </v-form>
-    <div class="text-center">
-      <v-btn color="primary" text class="mt-2" @click="updatePassword">
-        更新
-      </v-btn>
-    </div>
-    <v-snackbar v-model="snackbar.display" top min-height="40" height="40" :color="snackbar.color">
-      {{ snackbar.text }}
-    </v-snackbar>
-  </div>
+  <div />
 </template>
 
 <script>
 export default {
   data () {
     return {
-      valid: true,
-      password: {
-        current: '',
-        new: '',
-        confirm: ''
-      },
-      passwordDisplay: {
-        current: false,
-        new: false,
-        confirm: false
-      },
-      passwordRules: [
-        v => (v && v.length >= 5) || 'パスワードは6文字以上に設定してください。'
-      ],
-      passwordConfirmRules: [
-        v => (v === this.password.new) || 'パスワードが一致しません。'
-      ],
-      snackbar: {
-        display: false,
-        text: '',
-        color: 'primary'
-      }
     }
   },
   methods: {
-    validate () {
-      return this.$refs.form.validate()
-    },
-    updatePassword () {
-      if (this.validate()) {
-        this.password = {
-          current: '',
-          new: '',
-          confirm: ''
-        }
-        this.$store
-          .dispatch('api/apiRequest', {
-            api: 'passwordUpdate',
-            data: {
-              current: this.password.current,
-              new: this.new
-            }
-          }).then((res) => {
-            if (res.status === 200) {
-              this.snackbar = true
-              this.snackbarText = 'パスワードを更新しました'
-              this.snackbar = {
-                display: true,
-                text: 'パスワードを更新しました',
-                color: 'primary'
-              }
-            }
-          }).catch(() => {
-            this.snackbar = {
-              display: true,
-              text: 'パスワードの更新に失敗しました',
-              color: 'error'
-            }
-          })
-      }
-    }
   }
 }
 </script>
 <style lang="scss" scoped>
-.form-input {
-  border: solid 1px rgba(156, 156, 156, 0.37);
-  border-radius: 4px;
-  font-size: 14px;
-  width: 100%;
-}
-.reset-form {
-  display: grid;
-  grid-template-columns: 200px 1fr;
-  grid-gap: 8px 0;
-}
 </style>
