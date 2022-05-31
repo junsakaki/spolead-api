9d82c242b4b7f81a8f8b300da9acf1d4da8c7da4
j-kubo <j-kubo@keylink.co.jp>
2022-05-31 18:35:08 +0900

    update

--- components/shared/organisms/Header.vue
+++ components/shared/organisms/Header.vue
@@ -45,14 +45,14 @@
         >
       </router-link>
       <div class="links">
-        <router-link :to="`/lp.html`" class="link icon-button">
+        <a href="https://spoleader.com/lp.html">
           <v-icon>
             mdi-cursor-pointer
           </v-icon>
           <p class="icon-button-link">
             はじめての方へ
           </p>
-        </router-link>
+        </a>
         <button class="link icon-button align-center">
           <v-icon>
             mdi-account-group
