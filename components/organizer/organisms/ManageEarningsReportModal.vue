28fe86dccea073dd8be570ee1f33f98af5927344
ririki-nakabayashi <ririki.nakabayashi.force@gmail.com>
2022-07-30 22:47:56 +0900

    feat(organize): 管理画面内にある売上報告/出金履歴モーダルのレイアウトを作成

--- components/organizer/organisms/ManageEarningsReportModal.vue
+++ components/organizer/organisms/ManageEarningsReportModal.vue
@@ -1,15 +1,182 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-if="dialog" :value="dialog" persistent max-width="600px">
-      <v-card>
+  <v-row justify="space-around">
+    <v-dialog
+      v-if="dialog"
+      :value="dialog"
+      fullscreen
+      max-width="600px"
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-card class="d-flex align-start flex-column">
         <v-card-title class="justify-space-between">
-          <span class="headline">売上報告</span>
+          <span>売上報告</span>
         </v-card-title>
         <v-card-text>
-          総売上、サービス/サービス内商品/年月ごとの絞り込み
+          <v-dialog
+            ref="selectYearMonthModal"
+            v-model="modal"
+            :return-value.sync="selectedYearMonth"
+            persistent
+            width="290px"
+          >
+            <template #activator="{ on, attrs }">
+              <v-text-field
+                v-model="selectedYearMonth"
+                class="datepicker ml-auto"
+                label="期間を選択"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              />
+            </template>
+            <v-date-picker
+              v-model="selectedYearMonth"
+              type="month"
+              scrollable
+              locale="ja"
+            >
+              <v-spacer />
+              <v-btn
+                text
+                color="primary"
+                @click="modal = false"
+              >
+                キャンセル
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.selectYearMonthModal.save(selectedYearMonth)"
+              >
+                選択
+              </v-btn>
+            </v-date-picker>
+          </v-dialog>
+          <div class="d-flex flex-column">
+            <span>オンラインサロン</span>
+            <div class="earning-row pt-2">
+              <div class="head text-center grey--text">
+                累計売上
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(20000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                期間売上({{ modal ? '--' : format(new Date(`${selectedYearMonth}-01`), 'yyyy年MM月') }})
+              </div>
+              <div class="text-center ml-2">
+                {{ modal ? '--' : `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                サロンA
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                サロンB
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+          </div>
+          <div class="d-flex flex-column mt-8">
+            <span>クラウドファンディング</span>
+            <div class="earning-row pt-2">
+              <div class="head text-center grey--text">
+                累計売上
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(20000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                期間売上({{ modal ? '--' : format(new Date(`${selectedYearMonth}-01`), 'yyyy年MM月') }})
+              </div>
+              <div class="text-center ml-2">
+                {{ modal ? '--' : `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                ファンドA - プラン1
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                ファンドA - プラン2
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                ファンドB - プラン1
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                ファンドB - プラン2
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+          </div>
+          <div class="d-flex flex-column mt-4">
+            <span>指導者マッチング</span>
+            <div class="earning-row pt-2">
+              <div class="head text-center grey--text">
+                累計売上
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(20000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                期間売上({{ modal ? '--' : format(new Date(`${selectedYearMonth}-01`), 'yyyy年MM月') }})
+              </div>
+              <div class="text-center ml-2">
+                {{ modal ? '--' : `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                募集A
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+            <div class="earning-row pt-1">
+              <div class="head text-center grey--text">
+                募集B
+              </div>
+              <div class="text-center ml-2">
+                {{ `${(1000).toLocaleString()}円` }}
+              </div>
+            </div>
+          </div>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+        <v-card-actions class="mt-auto ml-auto justify-end">
           <v-btn color="blue darken-1" text @click="closeModal(false)">
             閉じる
           </v-btn>
@@ -20,16 +187,21 @@
 </template>
 
 <script>
+import { format } from 'date-fns'
+
 export default {
   props: {
     dialog: {
       type: Boolean,
       default: false
     }
-    // salon: {
-    //   type: Object,
-    //   default: null
-    // }
+  },
+  data () {
+    return {
+      selectedYearMonth: format(new Date(), 'yyyy-MM'),
+      format,
+      modal: false
+    }
   },
   created () {
   },
@@ -41,4 +213,14 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.datepicker {
+  max-width: 180px;
+}
+.earning-row {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  .head {
+    border-right: solid 1px;
+  }
+}
 </style>
--- components/organizer/organisms/ManageWithdrawalHistoryModal.vue
+++ components/organizer/organisms/ManageWithdrawalHistoryModal.vue
@@ -1,15 +1,50 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-if="dialog" :value="dialog" persistent max-width="600px">
-      <v-card>
+  <v-row justify="space-around">
+    <v-dialog
+      v-if="dialog"
+      :value="dialog"
+      fullscreen
+      max-width="600px"
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-card class="d-flex align-start flex-column">
         <v-card-title class="justify-space-between">
-          <span class="headline">出金履歴</span>
+          <span>出金履歴</span>
         </v-card-title>
         <v-card-text>
-          日時、金額、特定できるid？をテーブルで表示
+          <div class="withdrawal main mb-1">
+            <div class="text-center grey--text">
+              取引日時
+            </div>
+            <div class="text-center grey--text">
+              出金額
+            </div>
+            <div class="text-center grey--text">
+              取引内容
+            </div>
+          </div>
+          <v-divider class="mb-2" />
+          <div v-for="withdrawal in withdrawals" :key="`withdrawal-${withdrawal.id}`" class="withdrawal py-1">
+            <div class="withdrawal-name text-center ml-2">
+              {{ withdrawal.date }}
+            </div>
+            <div class="text-center">
+              {{ withdrawal.amount.toLocaleString() }}円
+            </div>
+            <div class="">
+              {{ withdrawal.token }}
+            </div>
+            <div class="filter" />
+          </div>
+          <div class="text-center mt-4">
+            <v-pagination
+              v-model="page"
+              :length="Math.ceil(total_rows / perPage)"
+            />
+          </div>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+        <v-card-actions class="mt-auto ml-auto justify-end">
           <v-btn color="blue darken-1" text @click="closeModal(false)">
             閉じる
           </v-btn>
@@ -26,10 +61,36 @@ export default {
       type: Boolean,
       default: false
     }
-    // salon: {
-    //   type: Object,
-    //   default: null
-    // }
+  },
+  data () {
+    return {
+      page: 1,
+      perPage: 20,
+      total_rows: 95,
+      withdrawals: [
+        { id: 0, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 1, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 2, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 3, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 4, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 5, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 6, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 7, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 8, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 9, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 10, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 11, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 12, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 13, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 14, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 15, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 16, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 17, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 18, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 19, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' },
+        { id: 20, date: '2022-01-01 12:00:00', amount: 100000, token: 'aidsd87a6d876a876687a / 四菱UFJ銀行 島根支店 口座番号: 119229600' }
+      ]
+    }
   },
   created () {
   },
@@ -41,4 +102,35 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.withdrawal {
+  position: relative;
+  display: grid;
+  grid-template-columns: 200px 150px 1fr;
+  text-decoration: none;
+  color: inherit;
+}
+.withdrawal:visited {
+  color: inherit;
+}
+.withdrawal.main {
+  font-size: 12px;
+}
+.withdrawal-name {
+  overflow: hidden;
+  width: 100%;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.withdrawal .filter {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #1976d214;
+  opacity: 0;
+  transition-duration: 0.5s;
+  cursor: pointer;
+}
+.withdrawal:hover .filter {
+  opacity: 1;
+}
 </style>
